0f4c469d1eb54c1f67a9ebf4106c2df17daeadc7
Grigorii <socratin28@gmail.com>
2024-08-20 17:47:37 +0400

    add param

--- tests/test_autorization.py
+++ tests/test_autorization.py
@@ -1,18 +1,20 @@
 import allure
+import pytest
 
 
+@pytest.mark.smoke
 @allure.title("Проверка токена на дееспособность")
 def test_is_live_token(token):
     token.get_new_token()
     token.check_response_token_is_correct()
 
-
+@pytest.mark.regression
 @allure.title("Проверка получени токена при имени  на кирилице")
 def test_get_new_token_cyrilic_name(token):
     token.get_new_token(name="Иван")
     token.check_response_token_is_correct()
 
-
+@pytest.mark.extended
 @allure.title("Проверка получени токена при имени спец символами")
 def test_get_new_token_special_name(token):
     token.get_new_token(name="!@#$%^&*()_+")
